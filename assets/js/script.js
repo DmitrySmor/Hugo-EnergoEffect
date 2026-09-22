d84a810cb2ba850b606341a4ea4e17c5a74a6af4
dmitrysmor <dmitrysmor@yandex.ru>
2026-09-22 10:41:34 +0300

    chore(ui): удалить неиспользуемую кнопку "Наверх"
    Кнопка больше не используется, удалена из разметки.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,6 @@
 function MobMenu() {
 	document.getElementById('mobmenu').classList.toggle('open')
 }
-let scrollbutton = document.getElementById('scroll-top')
-window.onscroll = function () {
-	scrollFunction()
-}
-
-function scrollFunction() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		scrollbutton.style.display = 'block'
-	} else {
-		scrollbutton.style.display = 'none'
-	}
-}
-
-scrollbutton.addEventListener('click', backToTop)
-
-function backToTop() {
-	document.body.scrollTop = 0
-	document.documentElement.scrollTop = 0
-}
 $('.main-carousel').owlCarousel({
 	loop: true,
 	margin: 15,
